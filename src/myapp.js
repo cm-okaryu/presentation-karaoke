9a18240a826269015ad1a452d98836ee4d857bed
ice <ise.yohei@classmethod.jp>
2019-12-11 20:08:27 +0900

    rename count to slideIndex, slide to slides

--- src/myapp.js
+++ src/myapp.js
@@ -1,8 +1,8 @@
 import { html, render } from 'lit-html'
 import { images } from './images.js'
 
-let count
-let slide
+let slideIndex
+let slides
 
 // 共通テンプレート
 const template = app => html`
@@ -14,13 +14,13 @@ const template = app => html`
 // 画像のINDEX表示
 const index = () => html`
   <div style="text-align: right">
-    ${count + 1}/5
+    ${slideIndex + 1}/5
   </div>
 `
 
 // 配列をインクリメントしてプレゼンページを再描画
 const nextImg = () => {
-  count++
+  slideIndex++
   renderApp()
 }
 
@@ -34,13 +34,13 @@ const img = src => html`
 
 // プレゼンページ表示
 const App = () => {
-  if (count > 4) {
+  if (slideIndex > 4) {
     return template(html`
       <p @click=${init}>終了！！！！</p>
     `)
   } else {
     return template(html`
-      ${index()} ${img(slide[count])}
+      ${index()} ${img(slides[slideIndex])}
     `)
   }
 }
@@ -67,8 +67,8 @@ const shuffleImages = imgs =>
 
 // 初期化して先頭ページ呼び出し
 const init = () => {
-  count = 0
-  slide = shuffleImages(images)
+  slideIndex = 0
+  slides = shuffleImages(images)
   startApp()
 }
 
