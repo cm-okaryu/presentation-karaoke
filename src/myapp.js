c2d712fe84f43378b69955407f5c1c5282ef39ec
ice <ise.yohei@classmethod.jp>
2019-12-11 19:26:32 +0900

    reorder functions

--- src/myapp.js
+++ src/myapp.js
@@ -4,20 +4,6 @@ import { images } from './images.js'
 let count
 let slide
 
-// 初期化して先頭ページ呼び出し
-const init = () => {
-  count = 0
-  slide = shuffleImages(images)
-  startApp()
-}
-
-// 配列をシャッフルする
-const shuffleImages = imgs =>
-  imgs
-    .map(a => [Math.random(), a])
-    .sort((a, b) => a[0] - b[0])
-    .map(a => a[1])
-
 // 共通テンプレート
 const template = app => html`
   <div>
@@ -25,14 +11,26 @@ const template = app => html`
   </div>
 `
 
-// 先頭ページ表示
-const startPage = () =>
-  template(html`
-    <p @click=${start}>プレゼン開始！！！！</p>
-  `)
+// 画像のINDEX表示
+const index = () => html`
+  <div style="text-align: right">
+    ${count + 1}/5
+  </div>
+`
 
-// カウントダウンを開始してプレゼンページ呼び出し
-const start = () => renderApp()
+// 配列をインクリメントしてプレゼンページを再描画
+const nextImg = () => {
+  count++
+  renderApp()
+}
+
+// プレゼン画像の表示
+const img = src => html`
+  <div @click=${nextImg} style="text-align: center;background: black;">
+    <img src="./images/${src}" height="600px" />
+    <div></div>
+  </div>
+`
 
 // プレゼンページ表示
 const App = () => {
@@ -47,28 +45,31 @@ const App = () => {
   }
 }
 
-// 画像のINDEX表示
-const index = () => html`
-  <div style="text-align: right">
-    ${count + 1}/5
-  </div>
-`
-// プレゼン画像の表示
-const img = src => html`
-  <div @click=${nextImg} style="text-align: center;background: black;">
-    <img src="./images/${src}" height="600px" />
-    <div></div>
-  </div>
-`
-
-// 配列をインクリメントしてプレゼンページを再描画
-const nextImg = () => {
-  count++
-  renderApp()
-}
-
 const renderApp = () => render(App(), document.body)
 
 const startApp = () => render(startPage(), document.body)
+
+// カウントダウンを開始してプレゼンページ呼び出し
+const start = () => renderApp()
+
+// 先頭ページ表示
+const startPage = () =>
+  template(html`
+    <p @click=${start}>プレゼン開始！！！！</p>
+  `)
+
+// 配列をシャッフルする
+const shuffleImages = imgs =>
+  imgs
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1])
+
+// 初期化して先頭ページ呼び出し
+const init = () => {
+  count = 0
+  slide = shuffleImages(images)
+  startApp()
+}
 
 init()
